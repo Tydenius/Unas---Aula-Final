ffe4d29caf6c19c36388545ec1c33a84df85c03a
Tydenius <victort.cordeiro@outlook.com.br>
2023-06-06 15:53:51 -0300

    update

---
+++ maptrip.py
@@ -0,0 +1,9 @@
+
+while True:
+    print("Bem-Vindo ao simulador de tarifas de viagens do Insper!")
+    print("\n ao digitar endereços, utilize o formato como o exemplo abaixo \n Rua Quata, 200, São Paulo \n Avenida Paulista, 560, São Paulo")
+    origem = input("Onde você está? \n")
+
+    destino = input("Qual o seu Destino? \n")
+
+    
